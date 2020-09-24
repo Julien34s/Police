22a4a5259b57d448efb712922a3ab921ad1bb7a9
Julien34s <70404496+Julien34s@users.noreply.github.com>
2020-09-24 22:46:35 +0200

    lieutenant

--- config.lua
+++ config.lua
@@ -261,29 +261,29 @@ Config.Uniforms = {
 		}
 	},
 
-	lieutenant = {
-		male = {
-			tshirt_1 = 58,  tshirt_2 = 0,
-			torso_1 = 55,   torso_2 = 0,
-			decals_1 = 8,   decals_2 = 2,
-			arms = 41,
-			pants_1 = 25,   pants_2 = 0,
-			shoes_1 = 25,   shoes_2 = 0,
-			helmet_1 = -1,  helmet_2 = 0,
-			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
-		},
-		female = {
-			tshirt_1 = 35,  tshirt_2 = 0,
-			torso_1 = 48,   torso_2 = 0,
-			decals_1 = 7,   decals_2 = 2,
-			arms = 44,
-			pants_1 = 34,   pants_2 = 0,
-			shoes_1 = 27,   shoes_2 = 0,
-			helmet_1 = -1,  helmet_2 = 0,
-			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
-		}
+	lieutenant_wear = { -- currently the same as intendent_wear
+	    male = {
+		    ['tshirt_1'] = 38,  ['tshirt_2'] = 1,
+		    ['torso_1'] = 143,   ['torso_2'] = 2,
+		    ['decals_1'] = 0,   ['decals_2'] = 0,
+		    ['arms'] = 41,
+		    ['pants_1'] = 52,   ['pants_2'] = 1,
+		    ['shoes_1'] = 25,   ['shoes_2'] = 0,
+		    ['helmet_1'] = 46,  ['helmet_2'] = 0,
+		    ['chain_1'] = 8,    ['chain_2'] = 0,
+		    ['ears_1'] = -1,     ['ears_2'] = 0
+	    },
+	    female = {
+		    ['tshirt_1'] = 38,  ['tshirt_2'] = 1,
+		    ['torso_1'] = 55,   ['torso_2'] = 0,
+		    ['decals_1'] = 0,   ['decals_2'] = 0,
+		    ['arms'] = 41,
+		    ['pants_1'] = 52,   ['pants_2'] = 1,
+		    ['shoes_1'] = 25,   ['shoes_2'] = 0,
+		    ['helmet_1'] = -1,  ['helmet_2'] = 0,
+		    ['chain_1'] = 0,    ['chain_2'] = 0,
+		    ['ears_1'] = 2,     ['ears_2'] = 0
+    	}
 	},
 
 	boss_wear = { -- currently the same as chef_wear
